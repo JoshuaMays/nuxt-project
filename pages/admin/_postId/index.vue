83249c45ea50f034a7b9d967db22764fa899c474
Joshua Mays <josh.mays@creditcards.com>
2018-03-10 20:45:12 -0600

    finishing section 3

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost"/>
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm';
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Joshua',
+        title: 'First Post!',
+        content: 'omg omg omg',
+        thumbnailLink: 'http://www.placecage.com/gif/200/300'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
+
